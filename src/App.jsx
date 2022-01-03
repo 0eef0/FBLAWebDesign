ec8cd27091654b5b590a3dccd9ca08baf0247376
TheCapn-MEC <erolda266@west-mec.org>
2022-01-02 20:14:44 -0700

    got react routing to work, got navbar mobile scaling workin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from './Components/Navbar';
+
+const getTempPage = (str) => {
+    return <h1>{str}</h1>
+};
 
 const App = () => {
     return (
-        <div>
-            <Navbar />
-        </div>
+        <Router>
+            <div>
+                <Navbar />
+                <p style={{height: '10vh'}} />
+                <Routes>
+                    <Route index element={getTempPage('Home')} />
+                    <Route path='contact' element={getTempPage('Contact')} />
+                    <Route path='about' element={getTempPage('About')} />
+                    <Route path='products' element={getTempPage('Product')} />
+                </Routes>
+            </div>
+        </Router>
     )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../Images/logo.png';
 
 const Navbar = () => {
+    const changeActive = (page) => {
+        const elems = document.getElementsByTagName('li');
+        for(let i = 0; i < elems.length; i++){
+            elems[i].classList.remove('active');
+        }
+        document.getElementById(page).classList.add('active');
+    }
+
     return (
         <nav>
             <ul>
-                <li className='active'><h1>Home</h1></li>
-                <li><h1>Contact</h1></li>
-                <li><h1>About</h1></li>
-                <li><h1>Products</h1></li>
+                <li className='active' onClick={() => {changeActive('home')}} id='home'><Link className='link' to='/'><h1>Home</h1></Link></li>
+                <li onClick={() => {changeActive('contact')}} id='contact'><Link className='link' to='/contact'><h1>Contact</h1></Link></li>
+                <li onClick={() => {changeActive('about')}} id='about'><Link className='link' to='/about'><h1>About</h1></Link></li>
+                <li onClick={() => {changeActive('products')}} id='products'><Link className='link' to='/products'><h1>Products</h1></Link></li>
             </ul>
             <img src={logo} alt="logo" />
         </nav>
