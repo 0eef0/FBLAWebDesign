6f474035ac3c22f1fd216a76f79af80b6edf1b81
Amakris12 <amakri386@west-mec.org>
2022-01-07 15:21:12 -0700

    Changed Color Scheme on Cards
    Changed some of the colors around on the card. I also added a button on the bottom to go to the other page its previewing.

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -12,43 +12,49 @@ const Products = () => {
                 <a className='link-Products' href="">
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Lorem, ipsum.</h3>
+                    <h3 className="Card-head">Website One</h3>
                     <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quae deleniti quia adipisci praesentium corporis!</p>
+                    <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
                 <a className='link-Products' href="">
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Lorem, ipsum.</h3>
+                    <h3 className="Card-head">Website Two</h3>
                     <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aspernatur itaque dignissimos aperiam! Corporis, possimus!</p>
+                    <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
                 <a className='link-Products' href="">
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Lorem, ipsum.</h3>
+                    <h3 className="Card-head">Website Three</h3>
                     <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid. Error placeat nisi esse saepe?</p>
+                    <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
                 <a className='link-Products' href="">
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Lorem, ipsum.</h3>
+                    <h3 className="Card-head">Website Four</h3>
                     <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quae deleniti quia adipisci praesentium corporis!</p>
+                    <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
                 <a className='link-Products' href="">
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Lorem, ipsum.</h3>
+                    <h3 className="Card-head">Website Five</h3>
                     <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aspernatur itaque dignissimos aperiam! Corporis, possimus!</p>
+                    <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
                 <a className='link-Products' href="">
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Lorem, ipsum.</h3>
+                    <h3 className="Card-head">Website Six</h3>
                     <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid. Error placeat nisi esse saepe?</p>
+                    <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
             </div>
