5060d8b9c625d15ecf8f3f27a97dd785c9512bf7
Amakris12 <amakri386@west-mec.org>
2022-01-10 14:54:37 -0700

    Added website to product page
    Added Minesweeper game from Ethan, and added a NBA page from my work

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -9,11 +9,11 @@ const Products = () => {
                 <p className="Pro-info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit aut quod eum nesciunt suscipit magnam tenetur saepe assumenda, cupiditate praesentium sint aspernatur eius quam nostrum dolore dolorem ex, qui earum?</p>
             </div>
             <div className="Card-Holder">
-                <a className='link-Products' href="">
+                <a className='link-Products' href="https://roldanreactminesweeper.netlify.app/">
                     <div className="Card">
-                    <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Website One</h3>
-                    <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quae deleniti quia adipisci praesentium corporis!</p>
+                    <img className="Card-Img" src="https://d2t1xqejof9utc.cloudfront.net/screenshots/pics/8f3b33a3ca5bc41d0e86a72f274306d8/large.png" alt="" />
+                    <h3 className="Card-head">Minesweeper</h3>
+                    <p className="Card-Info">This website was made by Ethan Ryan Roldan: This is a recreation of the Old Popular Game called Minesweeper, Enjoy!</p>
                     <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
@@ -21,7 +21,7 @@ const Products = () => {
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
                     <h3 className="Card-head">Website Two</h3>
-                    <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aspernatur itaque dignissimos aperiam! Corporis, possimus!</p>
+                    <p className="Card-Info">This website was made by Ethan Ryan Roldan</p>
                     <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
@@ -29,7 +29,7 @@ const Products = () => {
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
                     <h3 className="Card-head">Website Three</h3>
-                    <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid. Error placeat nisi esse saepe?</p>
+                    <p className="Card-Info">This website was made by Lily Zalit</p>
                     <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
@@ -37,7 +37,7 @@ const Products = () => {
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
                     <h3 className="Card-head">Website Four</h3>
-                    <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quae deleniti quia adipisci praesentium corporis!</p>
+                    <p className="Card-Info">This website was made by Lily Zalit</p>
                     <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
@@ -45,15 +45,15 @@ const Products = () => {
                     <div className="Card">
                     <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
                     <h3 className="Card-head">Website Five</h3>
-                    <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aspernatur itaque dignissimos aperiam! Corporis, possimus!</p>
+                    <p className="Card-Info">This website was made by Andrew Makris</p>
                     <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
-                <a className='link-Products' href="">
+                <a className='link-Products' href="https://tender-beaver-d56329.netlify.app/">
                     <div className="Card">
-                    <img className="Card-Img" src="https://media.istockphoto.com/photos/laptop-computer-with-blue-pink-lighting-and-blank-screen-place-on-picture-id1292038829?b=1&k=20&m=1292038829&s=170667a&w=0&h=L8DhcSOFS3BZdCHN6ldjumqQBw0J2kk7mBagO5io5jM=" alt="" />
-                    <h3 className="Card-head">Website Six</h3>
-                    <p className="Card-Info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, aliquid. Error placeat nisi esse saepe?</p>
+                    <img className="Card-Img" src="https://pbs.twimg.com/media/E6iDAlaWYAEF6ZP.jpg" alt="" />
+                    <h3 className="Card-head">NBA</h3>
+                    <p className="Card-Info">This website was made by Andrew Makris: This page is about the NBA and shows the NBA games scores, and NBA teams records. unfortunately The api that had been accessed is no longer available so its currently not up right now</p>
                     <button className="Card-Btn"><h4 className="Card-Btn-Text">Click To See</h4></button>
                 </div>
                 </a>
