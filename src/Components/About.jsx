4b81bb36c9d699eb9045777ca5605bc23aaffe0d
TheCapn-MEC <erolda266@west-mec.org>
2022-02-12 07:22:40 -0700

    added content for the competition

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Ethan from '../Images/Me.PNG';
+import Andrew from '../Images/Andrew Makris.jpg';
+import Lily from '../Images/Lily Zalit.jpg';
 
 const About = () => {
   return <div id='aboutPg'>
@@ -15,17 +17,17 @@ const About = () => {
         </div>
         <div className="founder">
           <p>
-            My name is Ethan Roldan. I am a full-stack web developer. I learned to code at the West-MEC Coding program from 2020-2022. Since then I've been working on a multitude of languages, including HTML, CSS, JS, React, and Node.js just to name a few examples.
+            My name is Andrew Makris and I am 18 and currently live in Phoenix Arizona. I just graduated this past year from Sandra Day O'Conner and now I am currently attending Glendale Community College to get my pre-requisites done for as cheap as possible. I am also in the West-Mec Coding program and I have been for the past year. I have enjoyed this class a lot and would like to go into a career path that involves coding
           </p>
-          <img src={Ethan} alt="Ethan Roldan" />
+          <img src={Andrew} alt="Andrew Makriss" />
         </div>
       </div>
       <div className="rightFounders">
         <div className="founder">
           <p>
-            My name is Ethan Roldan. I am a full-stack web developer. I learned to code at the West-MEC Coding program from 2020-2022. Since then I've been working on a multitude of languages, including HTML, CSS, JS, React, and Node.js just to name a few examples.
+            My name is Lily Zalit and I am mainly the designer of the group. I oversee colors, layouts, and the overall designs of all of the projects. I've also been going to West-MEC coding for a while to help me pursue my dream job of a UX Designer.
           </p>
-          <img src={Ethan} alt="Ethan Roldan" />
+          <img src={Lily} alt="Lily Zalit" />
         </div>
       </div>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import logo from '../Images/logo.png';
 
 const Home = () => {
     return (
         <div id='homePage'>
             <div id="left">
-
+                <img src={logo} alt='Logo' />
             </div>
             <div id="right">
                 <div className="right-box">
-
+                    <h1>Why we started WD</h1>
+                    <br />
+                    <p>
+                        We started this company because we all had one thing in common: we love to code. We all met in the West-MEC Northeast Coding Program and learned how to code there. Specifically, we learned how to develop websites. Since then, we've decided to hone our skills through WD.
+                    </p>
                 </div>
                 <div className="right-box">
-
+                    <h1>What does 'WD' mean?</h1>
+                    <br />
+                    <p>
+                        WD has two separate meanings. One is 'Web Development', so that we can get straight to the point with our business. The other one is 'West Development', so that we can represent where we learned our skills. Though, 'Development' is a constant because of our passion for the industry.
+                    </p>
                 </div>
             </div>
 
